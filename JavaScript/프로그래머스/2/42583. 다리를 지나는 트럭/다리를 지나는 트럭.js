d4a7d3dd036f7aa55f46d99f292cf68699b582a9
leeeyez <102468718+leeeyez@users.noreply.github.com>
2025-11-01 21:40:43 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 33.80 ms, Memory: 37.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ JavaScript/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -37,3 +37,29 @@ function solution(bridge_length, weight, truck_weights) {
     
     return time;
 }
+
+// 다리를 깊이 고정 배열로 정의하면 더 깔끔
+// time을 같이 관리해줄 필요X
+function solution(bridge_length, weight, truck_weights) {
+    let time = 0;
+    let bridge = Array(bridge_length).fill(0);
+    let bridgeWeight = 0;
+    
+    while (truck_weights.length > 0 || bridgeWeight > 0) {
+        time++;
+        
+        // 맨 앞 트럭 나감
+        bridgeWeight -= bridge.shift();
+        
+        // 새 트럭 들어올 수 있나?
+        if (truck_weights[0] && bridgeWeight + truck_weights[0] <= weight) {
+            let truck = truck_weights.shift();
+            bridge.push(truck);
+            bridgeWeight += truck;
+        } else {
+            bridge.push(0); // 빈 공간
+        }
+    }
+    
+    return time;
+}
