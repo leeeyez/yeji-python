3b69fba6f8d13c653751902f1a90e923cfa8ce5c
leeeyez <102468718+leeeyez@users.noreply.github.com>
2025-11-02 12:47:58 +0900

    [level 1] Title: 최소직사각형, Time: 26.50 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,17 @@
+function solution(sizes) {
+    var answer = 0;
+    
+    // 가로를 길게 하기
+    let sortSize = sizes.map(([x,y]) => {
+        if (x < y) return [y,x] 
+        else return [x,y]
+    })
+
+    let xSize = 0, ySize = 0;
+    for (let [x,y] of sortSize) {
+        xSize = Math.max(xSize, x);
+        ySize = Math.max(ySize, y);
+    }
+    
+    return xSize*ySize;
+}
