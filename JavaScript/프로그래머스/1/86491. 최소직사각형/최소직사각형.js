d3aeecded703d2ec22c00ee6b2c0b4588ad9130e
leeeyez <102468718+leeeyez@users.noreply.github.com>
2025-11-02 12:49:07 +0900

    [level 1] Title: 최소직사각형, Time: 9.00 ms, Memory: 41.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ JavaScript/프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -15,3 +15,9 @@ function solution(sizes) {
     
     return xSize*ySize;
 }
+
+// reduce로 푸는 방법
+// function solution(sizes) {
+//     const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
+//     return hor * ver;
+// }
